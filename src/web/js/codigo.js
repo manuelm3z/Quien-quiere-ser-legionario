522e7b799b3f9125ce335dafa6db8fc06f704af8
Manuel Zambrano <mmzmm3z@gmail.com>
2013-07-09 22:56:42 -0430

    arreglado seleccion de respuestas

--- src/web/js/codigo.js
+++ src/web/js/codigo.js
@@ -11,16 +11,42 @@ mensaje.prototype.alerta = function()
 {
 	alert(this.nombre);
 };
+/**
+* esta clase se encarga de cambiar estilos y clases
+*/
+var colores = function(){};
 
-var colores=function(){};
-colores.prototype.seleccionar(objeto)
+colores.prototype.seleccionar=function(objeto)
 {
-	console.log(objeto);
-	/*var clases=objeto.className, texto=clases.split(" "); 
-	if(texto.lastIndexOf("seleccionado")<0)
+	go=true;
+	//traigo todas las respuestas
+	opciones=document.getElementsByTagName('article');
+	for(var i=0; i<opciones.length; i++)
 	{
-		objeto.className=objeto.className+' seleccionado';
-	}*/
+		//verifico si alguna otra ya tiene la clase
+		if(opciones[i].id!=objeto.id)
+		{
+			if(opciones[i].className.split(" ").lastIndexOf("seleccionado")>0)
+			{
+				go=false;
+			}
+		}
+	}
+	if(go)
+	{
+		//Si la clase no esta la agrego
+		var posicion=objeto.className.split(" ").lastIndexOf("seleccionado");
+		if(posicion<0)
+		{
+			objeto.className=objeto.className+' seleccionado';
+		}
+		else if(posicion>0)
+		{
+			objeto.className=objeto.className.split(" ").slice(0,posicion).toString();
+		}
+	}
+	console.log(go);
+	console.log(posicion);
 }
 
 /**
